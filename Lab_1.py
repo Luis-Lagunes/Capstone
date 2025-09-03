34709ca876f605beb355d504b37845e7bc87b45d
LUIS_LAGUNES <LUIS_LAGUNES@gmail.com>
2025-09-03 00:20:08 -0500

    lab 1

--- Lab_1.py
+++ Lab_1.py
@@ -1,1 +1,21 @@
- 
+
+name = input('What is your name? ')
+letters = len(name)
+birthMonth = input('What month were you born in? ')
+
+print('Hello ' + name + ' nice to meet you!')
+print('Did you know that there are ' + str(letters) + ' letters in your first name?')
+
+if birthMonth == 'September':
+    print('Happy Birthday ' + name + '!')
+else:
+    print('Nice,' + birthMonth + ' is a great month!')
+
+classes = [] # creates an empty list
+
+count = int(input('How many classes are you taking this semester? ')) # counts the entered number
+for i in range(count): # creates a loop that keeps going until it reaches the entered number
+    class_names = input('Enter the name of class ' + str(i + 1) + ': ')
+    classes.append(class_names)
+
+print('Your classes are:', classes)
